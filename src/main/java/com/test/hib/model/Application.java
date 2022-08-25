5daa74ce2994bf9a0b8b96a26b4e2884657fa4fa
Gary J Howard <GaryJHoward@gmail.com>
2022-08-25 11:47:09 -0400

    added alternative implementation and moved tests to application class

--- src/main/java/com/test/hib/model/Application.java
+++ src/main/java/com/test/hib/model/Application.java
@@ -1,8 +1,30 @@
 
 package com.test.hib.model;
 
+import java.util.Scanner;
+
+import com.test.hib.controller.CreateUser;
+import com.test.hib.controller.DeleteUser;
+import com.test.hib.controller.UserTableActions;
+
 public class Application {
   public static void main(String[] args) {
-    
+	  		//this line calls the static method CreateUsers from the CreateUser Class
+			CreateUser.createUsers();
+			//this line calls the static method deleteUser from the DeleteUser Class
+	  		DeleteUser.deleteUser();
+	  		
+			//using the alternative non-static UserTableActions implementation. 
+			UserTableActions uta = new UserTableActions();
+			uta.createUsers();
+			//this gets the the user with the specified ID
+			System.out.print("Enter user ID to lookup:");
+			int i = 1;
+			try(Scanner sc = new Scanner(System.in))
+			{
+				 i = sc.nextInt();
+			}
+			System.out.println(uta.getUser(i).getFullname());
+			uta.close();
   }
 }
